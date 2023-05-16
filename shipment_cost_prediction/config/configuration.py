16c1c0cb8a1aafab7d07a957dccba5952bcef206
sumeet <maheshwarisumeet128@gmail.com>
2023-05-16 17:56:40 +0530

    Configuration file updated.

--- shipment_cost_prediction/config/configuration.py
+++ shipment_cost_prediction/config/configuration.py
@@ -64,6 +64,15 @@ class Configuration:
 
             data_validation_config = self.config_info[DATA_VALIDATION_CONFIG_KEY]
 
+            validated_path=os.path.join(data_validation_artifact_dir,
+                                        DATA_VALIDATION_VALID_DATASET)
+            
+            validated_train_path=os.path.join(data_validation_artifact_dir,
+                                              validated_path,DATA_VALIDATION_TRAIN_FILE)
+            
+            validated_test_path=os.path.join(data_validation_artifact_dir,
+                                             validated_path,DATA_VALIDATION_TEST_FILE)
+
             schema_file_path = os.path.join(ROOT_DIR,
                                             data_validation_config[DATA_VALIDATION_SCHEMA_DIR_KEY],
                                             data_validation_config[DATA_VALIDATION_SCHEMA_FILE_NAME_KEY])
@@ -75,6 +84,8 @@ class Configuration:
                                             data_validation_config[DATA_VALIDATION_REPORT_PAGE_FILE_NAME_KEY])
 
             data_validation_config = DataValidationConfig(schema_file_path=schema_file_path,
+                                                          validated_train_path=validated_train_path,
+                                                          validated_test_path=validated_test_path,
                                                           report_file_path=report_file_path,
                                                           report_page_file_path=report_page_file_path)
             logging.info(f"Data Validation config: {data_validation_config}")
