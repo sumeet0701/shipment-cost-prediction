b2e5bcf008b717d52bfd2ce51f3adde3e36816aa
sumeet <maheshwarisumeet128@gmail.com>
2023-05-27 08:48:00 +0530

    app.py code updated.

--- app.py
+++ app.py
@@ -5,22 +5,25 @@ from werkzeug.utils import secure_filename
 import os
 from shipment_cost_prediction.constant import * 
 
-
 input_file_path = "SCMS_Delivery_History_Dataset.csv"
 feature_engineering_file_path = "prediction_files/feat_eng.pkl"
 transformer_file_path = "prediction_files/preprocessed.pkl"
 model_file_path = "prediction_files/model.pkl"
 
+
+
+
 COUNTRY_MAP = {'Zambia': 0, 'Ethiopia': 1, 'Nigeria': 2, 'Tanzania': 3, "Côte d'Ivoire": 4, 'Mozambique': 5, 'Others': 6, 'Zimbabwe': 7, 'South Africa': 8, 'Rwanda': 9, 'Haiti': 10, 'Vietnam': 11, 'Uganda': 12}
 FULFILL_VIA_MAP = {'From RDC': 0, 'Direct Drop': 1}
 SHIPMENT_MODE_MAP = {'Truck': 0, 'Air': 1, 'Air Charter': 2, 'Ocean': 3}
 SUB_CLASSIFICATION_MAP = {'Adult': 0, 'Pediatric': 1, 'HIV test': 2, 'HIV test - Ancillary': 3, 'Malaria': 4, 'ACT': 5}
+DOSAGE_FORM= {'Tablet': 0, 'Test kit': 1, 'Oral': 2, 'Capsule': 3}
 BRAND_MAP = {'Generic': 0, 'Others': 1, 'Determine': 2, 'Uni-Gold': 3}
-FIRST_LINE_DESIGNATION_MAP = {'Yes': 0, 'No': 1}
+#FIRST_LINE_DESIGNATION_MAP = {'Yes': 0, 'No': 1}
 
 UPLOAD_FOLDER = 'batch_prediction/Uploaded_CSV_FILE'
 
-app = Flask(__name__, template_folder = 'template')
+app = Flask(__name__)
 ALLOWED_EXTENSIONS = {'csv'}
 
 @app.route("/", methods=["GET", "POST"])
@@ -58,20 +61,19 @@ def perform_batch_prediction():
 
 @app.route("/instance", methods=["POST"])
 def perform_instance_prediction():
-    pack_price = float(request.form['pack_price'])
-    unit_price = float(request.form['unit_price'])
     weight_kg = float(request.form['weight_kg'])
     line_item_quantity = int(request.form['line_item_quantity'])
+    line_item_value=float(request.form['line_item_value'])
     fulfill_via = request.form['fulfill_via']
     shipment_mode = request.form['shipment_mode']
     country = request.form['country']
     brand = request.form['brand']
     sub_classification = request.form['sub_classification']
-    first_line_designation = request.form['first_line_designation']
+    dosage_form=request.form['dosage_form']
 
-    predictor = instance_prediction_class(pack_price, unit_price, weight_kg, line_item_quantity,
+    predictor = instance_prediction_class(weight_kg, line_item_quantity,line_item_value,
                                                             fulfill_via, shipment_mode, country, brand,
-                                                            sub_classification, first_line_designation)
+                                                            sub_classification,dosage_form)
     predicted_price = predictor.predict_price_from_input()
 
     return render_template('index.html', prediction_type='instance', predicted_price=predicted_price)
